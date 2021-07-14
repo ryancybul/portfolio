06fa593abe2be8856e039b85e2ad90b99f8b9b67
Ryan Cybul <ryan.cybul@gmail.com>
2021-07-13 21:57:30 -0400

    removed wp portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import ArtBanner from '../components/artBanner';
-import WavySection from '../components/WavySection';
-import About from '../components/About';
-import Buzzwords from '../components/Buzzwords';
-import Portfolio from '../components/Portfolio';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import ArtBanner from "../components/artBanner";
+import WavySection from "../components/WavySection";
+import About from "../components/About";
+import Buzzwords from "../components/Buzzwords";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <ArtBanner />
     <About />
-    <Portfolio />
     <WavySection />
     <Buzzwords />
   </Layout>
