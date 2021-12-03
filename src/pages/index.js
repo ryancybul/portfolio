3c86c3cd5ca910b699a431149fa9ded0a5fb1bdd
Ryan Cybul <ryan.cybul@gmail.com>
2021-12-03 10:55:07 -0500

    Build error fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link, useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import ArtBanner from "../components/artBanner";
+import ArtBanner from "../components/ArtBanner";
 import WavySection from "../components/WavySection";
 import About from "../components/About";
 import Buzzwords from "../components/Buzzwords";
